b2de041be1d12a5dd9ce678b4a31eac6d451d14f
Jerry Linder <glz67@mst.edu>
2017-10-20 18:09:09 -0500

    seems to compile except for copy constructor error

--- change_notifier.cpp
+++ change_notifier.cpp
@@ -1,16 +1,66 @@
 #ifndef CHANGE_NOTIFIER
 #define CHANGE_NOTIFIER
 
+#include "change_notifier.h"
+
 /**
- * The change_notifier class is condition_variable that allows
- * threads to receive notifications even if they are not waiting
+ * The change_notifier class is basically a condition_variable that
+ * allows threads to receive notifications even if they are not waiting
  * when those notifications are sent.
  */
 
+notification_subscriber change_notifier::subscribe(unique_lock<mutex>& unique_l)
+{
+  subscriber_list.push_back(notification_subscriber(unique_l,*this));
+  subscriber_list.back().store_iterator(--subscriber_list.end());
+}
 
- //initialize notification_subscriber with unique_lock
- //keep track of subscribers with member function as cunstructor
- //and destructor that calls remove_subscriber
+void change_notifier::notify_all()
+{
+  for (sub_iter it = subscriber_list.begin(); it != subscriber_list.end(); it++)
+  {
+    (*it).notify_change();
+  }
+}
 
- //subscriber std::list
+void notification_subscriber::store_iterator(sub_iter i)
+{
+  it = i;
+}
+
+change_notifier::~change_notifier()
+{
+  subscriber_list.clear();
+}
+
+void notification_subscriber::wait()
+{
+  if (change_count == 0)
+  {
+    cv.wait(unique_l);
+  }
+  else
+  {
+    //no need to wait; a change has happened:
+    change_count--;
+  }
+}
+
+void notification_subscriber::notify_change()
+{
+  if (waiting_on_cv)
+  {
+    cv.notify_one();
+  }
+  else
+  {
+    change_count++;
+  }
+}
+//should be called at end of subscribing function (instead of destructor):
+void notification_subscriber::close()
+{
+  parent.subscriber_list.erase(it);
+}
+
 #endif
--- change_notifier.h
+++ change_notifier.h
@@ -1,4 +1,55 @@
 #ifndef CHANGE_NOTIFIER_H
 #define CHANGE_NOTIFIER_H
 
+#define sub_iter list<notification_subscriber>::iterator
+#include "change_notifier.h"
+#include <list>
+#include <condition_variable>
+#include <mutex>
+
+/**
+ * The change_notifier class is basically a condition_variable that
+ * allows threads to receive notifications even if they are not waiting
+ * when those notifications are sent.
+ */
+
+
+using namespace std;
+
+class change_notifier;
+
+class notification_subscriber
+{
+private:
+  condition_variable cv;
+  int change_count = 0;
+  unique_lock<mutex>& unique_l;
+  bool waiting_on_cv = false;
+  sub_iter it;
+  change_notifier& parent;
+public:
+  void wait();
+  void notify_change();
+  //note: please ONLY use the change_notifier::subscribe function
+  //and not this constructor; this class is not meant to be used alone:
+  notification_subscriber() = delete;
+  notification_subscriber(notification_subscriber&) = delete;
+  notification_subscriber(unique_lock<mutex>& ul, change_notifier& p) :
+    unique_l(ul), parent(p) {}
+  void close();
+  void store_iterator(sub_iter i);
+};
+
+class change_notifier
+{
+private:
+  list<notification_subscriber&> subscriber_list;
+  void store_iterator(sub_iter i);
+public:
+  notification_subscriber subscribe(unique_lock<mutex>& unique_l);
+  void notify_all();
+  friend void notification_subscriber::close();
+  ~change_notifier();
+};
+
 #endif
